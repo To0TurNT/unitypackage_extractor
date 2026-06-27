8a24e1bce165241b6f7511c3c27fbcd056319b36
TurNT <turnt441@gmail.com>
2026-06-27 12:21:23 -0400

    ci: testable 3.6-3.13 matrix + manual trigger; fork metadata in setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name='unitypackage_extractor',
     version='1.2.0',
-    description='Extractor for .unitypackage files',
-    long_description=open('README.md', 'r').read(),
+    description='Extractor for .unitypackage files (fork of Cobertos/unitypackage_extractor)',
+    long_description=open('README.md', 'r', encoding='utf-8').read(),
     long_description_content_type="text/markdown",
-    url='https://github.com/Cobertos/unitypackage_extractor/',
-    author='Cobertos',
-    author_email='me@cobertos.com',
+    url='https://github.com/To0TurNT/unitypackage_extractor/',
+    # Fork maintained by To0TurNT; original project by Cobertos (https://github.com/Cobertos)
+    author='To0TurNT',
     license='MIT',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
@@ -18,6 +18,10 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
         'Topic :: Software Development :: Build Tools',
         'Topic :: System :: Filesystems'
     ],
